620e9fc6a5be27e928ebf3d4e6d0343851bda752
Anthonyglory <anthonygloryy01@gmail.com>
2025-12-30 13:55:44 +0100

    exercise 1-5

---
+++ code.js
@@ -0,0 +1,48 @@
+//1 NAME VARIABLE
+
+let firstName = 'Glory';
+let lastName = 'Onojegborho';
+let age = 28;
+
+console.log('My name is ' + firstName + '  '+ lastName +   'and i am' + ' '+ age +  'years old. ')
+
+
+// 2 NUMBERS
+
+let num1 = 15;
+let num2 = 8;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 / num2);
+console.log(num1 * num2);
+
+
+// 3 if else statement
+
+let temperature =22
+if(temperature < 20){
+    console.log('its cold');
+}else{
+    console.log('its warm');
+}
+
+// 4 DISCOUNT PRIZE
+
+let price = 50;
+let discount = 10;
+let finalPrice = price - discount;
+
+console.log(finalPrice);
+
+/// 5 POSITIVE, NEGETIVE OR ZERO
+
+let number = -9;
+if(number < 0){
+    console.log('positive');
+}else if(number < 0){
+    console.log('negrtive');
+}else{
+    console.log('zero');
+}
+
