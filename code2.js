f55edf9171d32580878b0cbcae4ce6db9e679a51
Anthonyglory <anthonygloryy01@gmail.com>
2025-12-30 15:02:48 +0100

    exercise 6-10

---
+++ code2.js
@@ -0,0 +1,47 @@
+// 6 SCORE
+
+let score = 75;
+if(score >= 50){
+    console.log('pass');
+}else{
+    console.log('fail');
+}
+
+// 7 GREATER THAN
+
+let a = 12;
+let b = 15;
+if(a > b){
+    console.log('a is greater than b');
+}else{
+    console.log('a is not greater than b');
+}
+
+// 8 VOTE AGE
+
+let age = 16;
+let legalAge = 18;
+if(age >= legalAge){
+    console.log('can vote');
+}else{
+    console.log('can not vote');
+}
+
+// 9 WEEKEND
+let isWeekend = 'true';
+if(true){
+    console.log('No work today');
+}else{
+    console.log('time to work');
+    
+}
+
+
+// 10 EVEN OR ODD NUMBER
+
+let num = 22;
+if(num / 2 == 0){
+console.log('even');
+}else{
+    console.log('odd');
+}
