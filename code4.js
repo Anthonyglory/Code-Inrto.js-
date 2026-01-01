a4cc7b1f7a8a4fdb271d889fa0ccd7cfb3cd72eb
Anthonyglory <anthonygloryy01@gmail.com>
2026-01-01 09:22:11 +0100

    exercise 16-20

---
+++ code4.js
@@ -0,0 +1,49 @@
+//  16  CHARACTER CASE 
+
+let character = 'A';
+if(character >= 'A' && character <= 'Z'){
+    console.log('Uppercase letter');
+}else if(character <= 'A' && character <= 'Z'){
+    console.log('Lowercase letter');
+}else{
+    console.log('Not a letter');
+}
+
+
+// 17 DRIVING
+
+let ageToDrive = 25;
+let hasLicense = 'true';
+if(ageToDrive >= 18 && hasLicense == true){
+    console.log('can drive');
+}else{
+    console.log('cannot drive');
+}
+
+
+// 18 DISCOUNT
+
+
+
+
+
+
+// 19 DIVISABLE
+
+
+
+
+
+// 20 GREETINGS HOUR
+
+
+let hour = 14;
+if(hour >= 5 && hour <= 11){
+    console.log('Good Morning');
+}else if(hour >= 12 && hour <= 17){
+    console.log('Good Afternoon');
+}else if(hour >= 18 && hour <= 21 ){
+    console.log('Good Evening');
+}else{
+    console.log('Good Night');
+}
